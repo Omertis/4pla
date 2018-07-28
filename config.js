28ac2d7a65badfabe84e114123acc8fff76a7e35
Omertis <36013948+Omertis@users.noreply.github.com>
2018-07-28 13:26:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'NDQ3MTI1NTk1MTQzNDA1NTg5.DeDBrQ.7SyYYfZRj9xWUlyqKr_q5V1zjeY'; // ya wr3 la tns5
+exports.TOKEN = 'NDcxMjQwODYwNTIxMDcwNTk3.Dj3WDA.lTrhFSb0AKGYJrlgKj9ptmifGfU'; // ya wr3 la tns5
 
-exports.PREFIX = 'm';
+exports.PREFIX = '4';
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
